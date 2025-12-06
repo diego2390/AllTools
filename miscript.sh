fde4fd6d04650a3f6107262dca8daa31e7be55b8
HackerX <hackerx@anon.com>
2025-12-06 03:38:42 -0600

    Agregando script funcional

---
+++ miscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "======= MI SCRIPT PERSONAL ======="
+echo "Hola, este es mi script funcionando correctamente."
+echo "Fecha actual:"
+date
+echo "=================================="0
